129df47c56f8a432ee1ecbe39f2c97fe9d563ecf
wdr123 <32355709+wdr123@users.noreply.github.com>
2020-11-11 14:44:38 +0800

    Add files via upload

--- nornal_test.m
+++ nornal_test.m
@@ -1,13 +1,13 @@
 true_mean = 0.5;
 n_sample = 10000;
-fileID = fopen('uniform_log.txt','w+');
+fileID = fopen('norm_log.txt','w+');
 for i = 1:10
     fprintf(fileID,"Function %d 's log\n",i);
     for N = [10 100 1000 10000]
         hits = 0;
         mean_ep = 0;
         for reps = 1:n_sample
-            X = sample_uniform(N,0,1);
+            X = sample_normal(N,0,1);
             [A,B] = ci(X,i);
             ep = (B-A)/2;
             mean_ep = mean_ep + ep/n_sample;
